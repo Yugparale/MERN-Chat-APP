b2db6dba5d91df5858d35882a4a0334f201cc441
Yug Parale <yugparle@gmail.com>
2024-10-21 16:13:09 +0530

    Socket Url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:3000",{
+			const socket = io("https://mern-chat-app-67wi.onrender.com",{
 				query:{
 					userId:authUser._id
 				}
